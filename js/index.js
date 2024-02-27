243f96766e034ee7bfb83376c429592488da7026
Stanislau Vyrski <vyr4376@yandex.ru>
2024-02-27 20:11:38 +0300

    feat: added a feature to display a greeting depending on the time of day (Tue, Feb 27, 2024, 8:11:27 PM)

--- js/index.js
+++ js/index.js
@@ -20,7 +20,17 @@ const dateOptions = {
 const showDate = () => {
 	const currentDate = new Date().toLocaleDateString('en-US', dateOptions)
 	date.textContent = currentDate
-
 }
 
 showDate()
+
+const greetingText = document.querySelector('.main__greeting-text')
+const greetingArr = ['Good night, ', 'Good morning, ', 'Good afternoon, ', 'Good evening, ',]
+
+const showGreeting = () => {
+	const currentTime = new Date().getHours();
+	const getGreeting = greetingArr[Math.floor(currentTime / 6)]
+	greetingText.textContent = getGreeting
+}
+
+showGreeting()
