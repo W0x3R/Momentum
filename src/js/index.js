7f793d4d57be25d058e517ba5c0d509604061e2f
Stanislau Vyrski <vyr4376@yandex.ru>
2024-02-27 19:51:23 +0300

    feat:  created a function to display the current date (Tue, Feb 27, 2024, 7:51:16 PM)

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,24 @@ const time = document.querySelector('.main__data-time')
 const showTime = () => {
 	const currentTime = new Date().toLocaleTimeString()
 	time.textContent = currentTime
-	setInterval(showTime, 1000)
+	setInterval(() => {
+		showTime()
+		showDate()
+	}, 1000)
 }
 
-
 showTime()
+
+const date = document.querySelector('.main__data-date')
+const dateOptions = {
+	weekday: "long",
+	month: "long",
+	day: "numeric",
+}
+const showDate = () => {
+	const currentDate = new Date().toLocaleDateString('en-US', dateOptions)
+	date.textContent = currentDate
+
+}
+
+showDate()
