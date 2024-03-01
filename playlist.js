21156d5cd2b58d608c31331d5c68c087458160ab
Stanislau Vyrski <vyr4376@yandex.ru>
2024-03-01 17:49:10 +0300

    feat: added music tracks (Fri, Mar 1, 2024, 5:48:54 PM)

---
+++ playlist.js
@@ -0,0 +1,23 @@
+const playList = [
+	{
+		title: "Княzz - Адель",
+		src: "sounds/Княzz - Адель.mp3",
+		duration: "04:29",
+	},
+	{
+		title: "ДДТ - Дождь",
+		src: "sounds/ДДТ - Дождь.mp3",
+		duration: "05:04",
+	},
+	{
+		title: "Scorpions - Still loving you",
+		src: "sounds/Scorpions - Still loving you.mp3",
+		duration: "06:28",
+	},
+	{
+		title: "Imagine Dragons - Warriors",
+		src: "sounds/Imagine Dragons - Warriors.mp3",
+		duration: "02:50",
+	},
+];
+export default playList;
