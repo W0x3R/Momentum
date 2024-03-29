e804837f33de37f42f16c137cecb9f199943f581
Stanislau Vyrski <vyr4376@yandex.ru>
2024-03-29 16:29:35 +0300

    refactor: variable name changed (Fri, Mar 29, 2024, 4:29:29 PM)

--- src/js/components/greeting/showGreetingMessage.js
+++ src/js/components/greeting/showGreetingMessage.js
@@ -1,7 +1,8 @@
 import { getCurrentLanguage } from "../language/localStorageLanguage"
 
 const greetingText = document.querySelector('.greeting__text')
-const greetingObj = {
+
+const greetingObject = {
 	en: ['Good night, ', 'Good morning, ', 'Good afternoon, ', 'Good evening, '],
 	ru: ['Доброй ночи, ', 'Доброе утро, ', 'Добрый день, ', 'Добрый вечер, ']
 }
@@ -9,7 +10,7 @@ const greetingObj = {
 export const showGreetingText = () => {
 	const currentLanguage = getCurrentLanguage()
 	const currentTime = new Date().getHours();
-	const getGreeting = greetingObj[currentLanguage][Math.floor(currentTime / 6)]
+	const getGreeting = greetingObject[currentLanguage][Math.floor(currentTime / 6)]
 	greetingText.textContent = getGreeting
 	return getGreeting
 }
