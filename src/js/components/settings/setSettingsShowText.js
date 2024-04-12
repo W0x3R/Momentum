c30d3d7778bea4e4211065ce8aa79fa18a86273d
Stanislau Vyrski <vyr4376@yandex.ru>
2024-04-12 17:16:45 +0300

    fix: the property name in the settingsshowobj object has been changed (Fri, Apr 12, 2024, 5:16:41 PM)

--- src/js/components/settings/setSettingsShowText.js
+++ src/js/components/settings/setSettingsShowText.js
@@ -2,7 +2,7 @@ const settingsShowTitles = document.querySelectorAll('.settings__show p')
 
 const settingsShowObj = {
 	en: ['Show', 'Player', 'Weather', 'Clock', 'Date', 'Greeting', 'Quotes'],
-	ru: ['Отобразить', 'Плеер', 'Погода', 'Часы', 'Дата', 'Приветствие', 'Цитата']
+	ru: ['Показать', 'Плеер', 'Погода', 'Часы', 'Дата', 'Приветствие', 'Цитата']
 }
 
 export const setSettingsShowText = (lang) => {
