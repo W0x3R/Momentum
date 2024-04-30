c9a9e4a89e5a43d9541b0feac485ca58b76131a7
Stanislau Vyrski <vyr4376@yandex.ru>
2024-04-30 18:33:34 +0300

    fix: now in the module localstoragegreeting.js textcontent is used instead of value properties, because the input has been changed to span (Tue, Apr 30, 2024, 6:33:31 PM)

--- src/js/components/greeting/localStorageGreeting.js
+++ src/js/components/greeting/localStorageGreeting.js
@@ -1,9 +1,9 @@
 export const greetingName = document.querySelector('.greeting__name')
 
 export const setStorageGreetingName = () => {
-	localStorage.setItem('name', greetingName.value)
+	localStorage.setItem('name', greetingName.textContent)
 }
 
 export const getStorageGreetingName = () => {
-	greetingName.value = localStorage.getItem('name') || ''
+	greetingName.textContent = localStorage.getItem('name')
 }
