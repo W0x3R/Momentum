ede2caa4b8c8aa89dc91aa6dd23f385a474d7c07
Stanislau Vyrski <vyr4376@yandex.ru>
2024-04-30 18:32:00 +0300

    feat: renamed the module name and changed the setgreetingname function (Tue, Apr 30, 2024, 6:31:57 PM)

---
+++ src/js/components/greeting/setGreetingName.js
@@ -0,0 +1,16 @@
+import { greetingName } from "./localStorageGreeting"
+import { setStorageGreetingName } from "./localStorageGreeting"
+
+const greetingObj = {
+	en: '[Enter name]',
+	ru: '[Введите имя]'
+}
+
+export const setGreetingName = (lang) => {
+	const name = localStorage.getItem('name')
+	if (!name
+		|| greetingObj.en === name || greetingObj.ru === name || name === '') {
+		greetingName.textContent = greetingObj[lang]
+		setStorageGreetingName()
+	}
+}
