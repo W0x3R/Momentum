41b35c80894108d6f3754b4ce397ddfb6375b9aa
Stanislau Vyrski <vyr4376@yandex.ru>
2024-05-15 12:59:39 +0300

    refactor: the checkismute function has been moved from the volume module to the new checkismute module (Wed, May 15, 2024, 12:58:34 PM)

---
+++ src/js/components/player/checkIsMute.js
@@ -0,0 +1,8 @@
+import { getStorageVolumeValue } from "./localStoragePlayer"
+import { isMute } from "./setMute"
+import { setVolumeBtnIcon } from "./setVolumeBtnIcon"
+
+export const checkIsMute = () => {
+	const volumeValue = getStorageVolumeValue()
+	isMute ? setVolumeBtnIcon(volumeValue, 'muteSongBtn.svg#muteSong', false) : setVolumeBtnIcon(0, 'unMuteSongBtn.svg#unMuteSong', true)
+}
--- src/js/components/player/volume.js
+++ src/js/components/player/volume.js
@@ -1,15 +1,9 @@
 import { audio } from "./playbackControl/playSong";
 import { setStorageVolumeValue, getStorageVolumeValue } from "./localStoragePlayer";
-import { isMute, setIsMute } from "./setMute";
+import { setIsMute } from "./setMute";
 import { setMuteBtnHref } from "./setMuteBtnHref";
-import { setVolumeBtnIcon } from "./setVolumeBtnIcon";
 
 export const volumeBtn = document.querySelector('.player__sounds-volume')
-
-export const checkIsMute = () => {
-	const volumeValue = getStorageVolumeValue()
-	isMute ? setVolumeBtnIcon(volumeValue, 'muteSongBtn.svg#muteSong', false) : setVolumeBtnIcon(0, 'unMuteSongBtn.svg#unMuteSong', true)
-}
 
 export const setVolumeBtnValue = () => {
 	const volumeValue = getStorageVolumeValue()
