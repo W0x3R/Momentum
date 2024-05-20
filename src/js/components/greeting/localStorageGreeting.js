8ed2cc7799b6d4266289db7761a9403ea2727c48
Stanislau Vyrski <vyr4376@yandex.ru>
2024-05-20 12:53:31 +0300

    refactor: in the localstoragegreeting module, the function getstoragegreetingname has been changed.it now performs actions according to its name. (Mon, May 20, 2024, 12:53:19 PM)

--- src/js/components/greeting/localStorageGreeting.js
+++ src/js/components/greeting/localStorageGreeting.js
@@ -1,9 +1,4 @@
 export const greetingName = document.querySelector('.greeting__name')
 
-export const setStorageGreetingName = () => {
-	localStorage.setItem('name', greetingName.textContent)
-}
-
-export const getStorageGreetingName = () => {
-	greetingName.textContent = localStorage.getItem('name')
-}
+export const setStorageGreetingName = () => localStorage.setItem('name', greetingName.textContent)
+export const getStorageGreetingName = () => localStorage.getItem('name')
