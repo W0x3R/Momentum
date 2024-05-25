edf482ed6176000439888baac6f2cb91845fc4a0
Stanislau Vyrski <vyr4376@yandex.ru>
2024-05-25 20:04:35 +0300

    fix: changed the setvolumebtnicon function.now when you click on the song muta icon in the sound volume input, the value does not change (Sat, May 25, 2024, 8:04:29 PM)

--- js/index.js
+++ js/index.js
@@ -2362,19 +2362,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   setVolumeBtnIcon: function() { return /* binding */ setVolumeBtnIcon; }
 /* harmony export */ });
 /* harmony import */ var _playbackControl_playSong__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56);
-/* harmony import */ var _setVolumeBtnValue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(76);
-/* harmony import */ var _setMuteBtnHref__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(78);
-/* harmony import */ var _setMute__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(77);
-
+/* harmony import */ var _setMuteBtnHref__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(78);
+/* harmony import */ var _setMute__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(77);
 
 
 
 
 const setVolumeBtnIcon = (volumeValue, iconName, isMuteValue) => {
 	_playbackControl_playSong__WEBPACK_IMPORTED_MODULE_0__.audio.volume = volumeValue
-	_setVolumeBtnValue__WEBPACK_IMPORTED_MODULE_1__.volumeInput.value = volumeValue
-	;(0,_setMuteBtnHref__WEBPACK_IMPORTED_MODULE_2__.setMuteBtnHref)(iconName)
-	;(0,_setMute__WEBPACK_IMPORTED_MODULE_3__.setIsMute)(isMuteValue)
+	;(0,_setMuteBtnHref__WEBPACK_IMPORTED_MODULE_1__.setMuteBtnHref)(iconName)
+	;(0,_setMute__WEBPACK_IMPORTED_MODULE_2__.setIsMute)(isMuteValue)
 }
 
 /***/ }),
--- src/js/components/player/volume/setVolumeBtnIcon.js
+++ src/js/components/player/volume/setVolumeBtnIcon.js
@@ -1,11 +1,9 @@
 import { audio } from "../playbackControl/playSong"
-import { volumeInput } from "./setVolumeBtnValue"
 import { setMuteBtnHref } from "./setMuteBtnHref"
 import { setIsMute } from "./setMute"
 
 export const setVolumeBtnIcon = (volumeValue, iconName, isMuteValue) => {
 	audio.volume = volumeValue
-	volumeInput.value = volumeValue
 	setMuteBtnHref(iconName)
 	setIsMute(isMuteValue)
 }
