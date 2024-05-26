e695ab36840a2d11ba1a5054a13ebed71a0d8e17
Stanislau Vyrski <vyr4376@yandex.ru>
2024-05-26 15:12:21 +0300

    feat: added check for presence of localstorage.getitem('city') value to getstoragecity function (Sun, May 26, 2024, 3:12:07 PM)

--- src/js/components/weather/localStorageWeather.js
+++ src/js/components/weather/localStorageWeather.js
@@ -1,4 +1,4 @@
 import { weatherInput } from "./getWeather"
 
 export const setStorageCity = () => localStorage.setItem('city', weatherInput.value)
-export const getStorageCity = () => localStorage.getItem('city')
+export const getStorageCity = () => localStorage.getItem('city') || 'Гомель'
