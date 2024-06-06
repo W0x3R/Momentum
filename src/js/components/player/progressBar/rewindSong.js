004c94339d2b3f49cebcdb325ee40ea8c1be0b84
Stanislau <vyr4376@yandex.ru>
2024-06-06 14:47:48 +0300

    fix: the rewindsong function has been changed (Thu, Jun 6, 2024, 2:47:45 PM)

--- src/js/components/player/progressBar/rewindSong.js
+++ src/js/components/player/progressBar/rewindSong.js
@@ -2,9 +2,7 @@ import { audio } from "../playbackControl/playSong";
 import { progressBar } from "./updateProgressBar";
 
 export const rewindSong = () => {
-	const value = parseFloat(progressBar.value);
-	const max = parseFloat(progressBar.max);
-	if (!isNaN(value) && !isNaN(max)) {
-		audio.currentTime = (value / max) * audio.duration;
-	}
+	const value = progressBar.value;
+	const max = progressBar.max;
+	audio.currentTime = (value / max) * audio.duration;
 }
